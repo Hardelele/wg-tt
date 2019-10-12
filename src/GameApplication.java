d50d06137f57151c92e3159112971cd63d55eecb
Hardelele <i.lapshina@mail.ru>
2019-10-12 07:06:59 +0300

    Add GameApplication.java Main class with main method.

---
+++ src/GameApplication.java
@@ -0,0 +1,11 @@
+public class GameApplication {
+
+    public static void main(String[] args) {
+
+        ConsoleVisualizer consoleVisualizer = new ConsoleVisualizer();
+
+        consoleVisualizer.printGameField();
+
+    }
+
+}
