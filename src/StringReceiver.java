f92005b7c9111e1b3f09fb1c350bc93ca9a57c39
Hardelele <i.lapshina@mail.ru>
2019-10-12 07:20:19 +0300

    Add StringReceiver.java Interface that operates with Scanner object. Have to implements in ConsoleController.

---
+++ src/StringReceiver.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+/*
+ TODO:: Сделать printExceptions методы.
+ */
+
+public class StringReceiver {
+    public static String input() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+}
+
