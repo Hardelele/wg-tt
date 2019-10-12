7ac9cf569cdc9e32786ab070f92c297914c2cd4d
Hardelele <i.lapshina@mail.ru>
2019-10-12 07:01:09 +0300

    Add ColorsCollector.java Contains const values.

---
+++ src/ColorsCollector.java
@@ -0,0 +1,11 @@
+public class ColorsCollector {
+
+    // TODO:: применить во всех классах, везде где это возможно.
+
+    public static final String RED = "red";
+
+    public static final String BLUE = "blue";
+
+    public static final String PURPLE = "purple";
+
+}
